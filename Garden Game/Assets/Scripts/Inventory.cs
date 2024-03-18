e2166c9080ee01055a5162ec03e7ecb65949cb1f
rlawson04 <rbl2991@rit.edu>
2024-03-18 13:37:11 -0400

    Shop Appears and Dissapears

--- Garden Game/Assets/Scripts/Inventory.cs
+++ Garden Game/Assets/Scripts/Inventory.cs
@@ -1,22 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class Inventory : MonoBehaviour
 {
     public bool selected;
+    [SerializeField] private GameObject canvas;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        canvas.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (selected)
-        {
 
-        }
+    }
+
+    public void ButtonPressed()
+    {
+        selected = !selected;
+        canvas.SetActive(selected);
     }
 }
