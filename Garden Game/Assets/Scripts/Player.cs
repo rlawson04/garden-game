c946d380ad40161f0eac9d4accc20802b64fa33d
rlawson04 <rbl2991@rit.edu>
2024-03-18 15:22:35 -0400

    Working on player movement and camera

---
+++ Garden Game/Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] private GameObject mainCamera;
+    [SerializeField] private GameObject playerCamera;
+    [SerializeField] private GameObject player;
+
+    private bool selected;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        selected = true;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            selected = !selected;
+        }
+        mainCamera.SetActive(selected);
+        playerCamera.SetActive(!selected);
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            player.transform.position += Vector3.forward;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            player.transform.position -= Vector3.forward;
+        }
+    }
+}
