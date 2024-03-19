f998b27b4f5e999eac779649492f29175a923fc9
rlawson04 <rbl2991@rit.edu>
2024-03-18 22:43:22 -0400

    Revamping movement with camera

--- Garden Game/Assets/Scripts/PlayerController.cs
+++ Garden Game/Assets/Scripts/PlayerController.cs
@@ -4,41 +4,26 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
-    [SerializeField] private GameObject _mainCamera;
-    [SerializeField] private GameObject _playerCamera;
-    [SerializeField] private Rigidbody _rb;
     [SerializeField] private float _speed = 1;
     [SerializeField] private float _jumpForce = 200;
-
-
-
-    private bool isSelected;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        isSelected = true;
-        
-    }
+    public CharacterController controller;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.C))
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        
+        if(direction.magnitude > 0.1f)
         {
-            isSelected = !isSelected;
+            controller.Move(direction * _speed * Time.deltaTime);
         }
-        _mainCamera.SetActive(isSelected);
-        _playerCamera.SetActive(!isSelected);
-
-        var vel = new Vector3(Input.GetAxis("Horizontal") * _speed, 0, Input.GetAxis("Vertical") * _speed);
-        vel.y = _rb.velocity.y;
-        _rb.velocity = vel;
-
+        
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            _rb.AddForce(Vector3.up * _jumpForce);
+         
         }
         
     }
