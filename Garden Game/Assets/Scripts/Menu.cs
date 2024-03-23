5f11f76d93a7da7e514ce9a08d4f7d3b240876d9
rlawson04 <rbl2991@rit.edu>
2024-03-23 16:31:11 -0400

    Menu and Inventory Working

--- Garden Game/Assets/Scripts/Menu.cs
+++ Garden Game/Assets/Scripts/Menu.cs
@@ -1,34 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class Inventory : MonoBehaviour
+
+public class Menu : MonoBehaviour
 {
     public bool selected;
-    [SerializeField] private GameObject inventoryCanvas;
+    [SerializeField] private GameObject menuCanvas;
     [SerializeField] private GameObject uiCanvas;
 
     // Start is called before the first frame update
     void Start()
     {
-        inventoryCanvas.SetActive(false);
+        menuCanvas.SetActive(false);
         uiCanvas.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.I))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             selected = !selected;
-            inventoryCanvas.SetActive(selected);
+            menuCanvas.SetActive(selected);
             uiCanvas.SetActive(!selected);
         }
-       
+
     }
-
-   
-        
-    
-
-
 }
