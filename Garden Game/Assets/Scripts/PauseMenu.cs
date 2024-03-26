07b6a1befce1680ba3ffa8a90ff6782f19769e71
rlawson04 <rbl2991@rit.edu>
2024-03-26 13:47:21 -0400

    Pause Menu working

---
+++ Garden Game/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isPaused = false;
+    [SerializeField] private GameObject menuCanvas;
+    [SerializeField] private GameObject shopCanvas;
+    [SerializeField] private GameObject teleportCanvas;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (menuCanvas.activeInHierarchy || shopCanvas.activeInHierarchy || teleportCanvas.activeInHierarchy)
+        {
+            Pause();
+        }
+    }
+
+    public void Resume()
+    {
+        isPaused = false;
+        Time.timeScale = 1f;
+    }
+    void Pause()
+    {
+        isPaused = true;
+        Time.timeScale = 0f;
+
+    }
+}
